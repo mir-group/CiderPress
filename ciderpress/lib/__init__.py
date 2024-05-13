5d7a0d650e8ea4b3816fed71b5dc431e4f38bde3
kylebystrom <kylebystrom@gmail.com>
2024-05-13 14:35:34 -0400

    remove old analyzers and density modules, fix lint

--- ciderpress/lib/__init__.py
+++ ciderpress/lib/__init__.py
@@ -2,6 +2,8 @@ import ctypes
 
 from ciderpress.lib.load import load_library
 
+__all__ = ["load_library", "c_double_p", "c_int_p", "c_null_ptr"]
+
 c_double_p = ctypes.POINTER(ctypes.c_double)
 c_int_p = ctypes.POINTER(ctypes.c_int)
 c_null_ptr = ctypes.POINTER(ctypes.c_void_p)
