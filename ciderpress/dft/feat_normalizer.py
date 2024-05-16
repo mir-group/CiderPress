fix numerical stability of gaussian interpolation, continue adding GGA-level NLDF features

ciderpress/dft/feat_normalizer.py
@@ -260,20 +260,40 @@ class FeatNormalizerList:
         if x.shape[-2] != self.nfeat:
             raise ValueError("Array must have size nfeat")
 
+    def _get_rho_and_inh(self, X0T):
+        rho_term = X0T[:, 0]
+        grad_term = X0T[:, 1]
+        tau_term = X0T[:, 2]
+        if self.slmode == "npa":
+            inh_term = tau_term + 5.0 / 3 * grad_term
+        elif self.slmode == "nst":
+            inh_term = tau_term / (CFC * rho_term ** (5.0 / 3))
+        elif self.slmode == "np":
+            inh_term = 5.0 / 3 * grad_term
+        else:
+            inh_term = grad_term / (8 * CFC * rho_term ** (8.0 / 3))
+        rho_term = np.maximum(rho_term, self.cutoff)
+        return rho_term, inh_term
+
+    def _get_drho_and_dinh(self, X0T, DX0T):
+        drho = DX0T[0]
+        grad_term = DX0T[1]
+        tau_term = DX0T[2]
+        if self.slmode == "npa":
+            dinh = tau_term + 5.0 / 3 * grad_term
+        elif self.slmode == "nst":
+            dinh = 0  # TODO
+        elif self.slmode == "np":
+            dinh = 5.0 / 3 * grad_term
+        return drho, dinh
+
     def get_normalized_feature_vector(self, X0T):
         # This function should be in the settings class, rather than
         # a plan, because it is called from models, which do not
         # require knowledge of the plan.
         self._check_shape(X0T)
         X0TN = np.empty_like(X0T)
-        rho_term = X0T[:, 0]
-        grad_term = X0T[:, 1]
-        tau_term = X0T[:, 2]
-        if self.slmode == "npa":
-            inh_term = tau_term + 5.0 / 3 * grad_term
-        else:
-            inh_term = tau_term
-        rho_term = np.maximum(rho_term, self.cutoff)
+        rho_term, inh_term = self._get_rho_and_inh(X0T)
         nfeat = self.nfeat
         for i in range(nfeat):
             if self[i] is not None:
@@ -286,13 +306,8 @@ class FeatNormalizerList:
         self._check_shape(X0T, ndim=2)
         self._check_shape(DX0T, ndim=2)
         DX0TN = np.empty_like(DX0T)
-        rho = X0T[0]
-        grad_term = X0T[1]
-        tau_term = X0T[2]
-        if self.slmode == "npa":
-            inh = tau_term + 5.0 / 3 * grad_term
-        else:
-            inh = tau_term
+        rho, inh = self._get_rho_and_inh(X0T[None, :])
+        rho, inh = rho[0], inh[0]
         drho = DX0T[0]
         grad_term = DX0T[1]
         tau_term = DX0T[2]

ciderpress/dft/lcao_nldf_generator.py
@@ -87,13 +87,11 @@ class LCAONLDFGenerator:
         idx_map = self.grids_indexer.idx_map
         ngrids = self.grids_indexer.idx_map.size
 
-        sigma_in = np.einsum("xg,xg->g", rho_in[1:4], rho_in[1:4])
-        arg_in_g = self.plan.get_interpolation_arguments(
-            rho_in[0], sigma_in, rho_in[4], i=-1
-        )
-        func_in_g = self.plan.get_function_to_convolve(rho_in[0], sigma_in, rho_in[4])
-        arg_in_g, darg_in_g = arg_in_g[0], arg_in_g[1:]
-        func_in_g, dfunc_in_g = func_in_g[0], func_in_g[1:]
+        rho_tuple = self.plan.get_rho_tuple(rho_in)
+        arg_in_g = self.plan.get_interpolation_arguments(rho_tuple, i=-1)
+        func_in_g = self.plan.get_function_to_convolve(rho_tuple)
+        arg_in_g, darg_in_g = arg_in_g[0], arg_in_g[1]
+        func_in_g, dfunc_in_g = func_in_g[0], func_in_g[1]
         arg_g = np.zeros(ngrids_ato)
         func_g = np.zeros(ngrids_ato)
         arg_g[idx_map] = arg_in_g[:ngrids]
@@ -107,9 +105,7 @@ class LCAONLDFGenerator:
         # do contractions to get features
         feat, dfeat = self.plan.eval_rho_full(
             f_gq,
-            rho_in[0],
-            rho_in[1:4],
-            rho_in[4],
+            rho_in,
             apply_transformation=False,
             spin=spin,
         )
@@ -138,13 +134,11 @@ class LCAONLDFGenerator:
         idx_map = self.grids_indexer.idx_map
         ngrids = self.grids_indexer.idx_map.size
 
-        sigma_in = np.einsum("xg,xg->g", rho_in[1:4], rho_in[1:4])
-        arg_in_g = self.plan.get_interpolation_arguments(
-            rho_in[0], sigma_in, rho_in[4], i=-1
-        )
-        func_in_g = self.plan.get_function_to_convolve(rho_in[0], sigma_in, rho_in[4])
-        arg_in_g, darg_in_g = arg_in_g[0], arg_in_g[1:]
-        func_in_g, dfunc_in_g = func_in_g[0], func_in_g[1:]
+        rho_tuple = self.plan.get_rho_tuple(rho_in)
+        arg_in_g = self.plan.get_interpolation_arguments(rho_tuple, i=-1)
+        func_in_g = self.plan.get_function_to_convolve(rho_tuple)
+        arg_in_g, darg_in_g = arg_in_g[0], arg_in_g[1]
+        func_in_g, dfunc_in_g = func_in_g[0], func_in_g[1]
         arg_g = np.zeros(ngrids_ato)
         func_g = np.zeros(ngrids_ato)
         arg_g[idx_map] = arg_in_g[:ngrids]
@@ -156,25 +150,20 @@ class LCAONLDFGenerator:
         f_gq = self._perform_fwd_convolution(theta_gq)
         start = 0 if self.plan.nldf_settings.nldf_type == "i" else self.plan.nalpha
         start += len(self.plan.nldf_settings.l0_feat_specs)
-        feat = self.plan.eval_rho_full(
-            f_gq, rho_out[0], rho_out[1:4], rho_out[4], apply_transformation=False
-        )[0]
+        feat = self.plan.eval_rho_full(f_gq, rho_out, apply_transformation=False)[0]
         occd_feats = []
 
         for iorb in range(orb_rho_in_iorb.shape[0]):
             orb_rho_in = orb_rho_in_iorb[iorb]
             orb_rho_out = orb_rho_out_iorb[iorb]
             occd_sigma_in = 2 * np.einsum("xg,xg", rho_in[1:4], orb_rho_in[1:4])
-            occd_arg_in_g = (
-                orb_rho_in[0] * darg_in_g[0]
-                + occd_sigma_in * darg_in_g[1]
-                + orb_rho_in[4] * darg_in_g[2]
-            )
+            occd_arg_in_g = orb_rho_in[0] * darg_in_g[0] + occd_sigma_in * darg_in_g[1]
             occd_func_in_g = (
-                orb_rho_in[0] * dfunc_in_g[0]
-                + occd_sigma_in * dfunc_in_g[1]
-                + orb_rho_in[4] * dfunc_in_g[2]
+                orb_rho_in[0] * dfunc_in_g[0] + occd_sigma_in * dfunc_in_g[1]
             )
+            if self.plan.nldf_settings.sl_level == "MGGA":
+                occd_arg_in_g[:] += orb_rho_in[4] * darg_in_g[2]
+                occd_func_in_g[:] += orb_rho_in[4] * dfunc_in_g[2]
             occd_arg_g = np.zeros(ngrids_ato)
             occd_func_g = np.zeros(ngrids_ato)
             occd_arg_g[idx_map] = occd_arg_in_g[:ngrids]
@@ -186,13 +175,9 @@ class LCAONLDFGenerator:
             occd_f_gq = self._perform_fwd_convolution(occd_theta_gq)
             occd_feat = self.plan.eval_occd_full(
                 f_gq,
-                rho_out[0],
-                rho_out[1:4],
-                rho_out[4],
+                rho_out,
                 occd_f_gq,
-                orb_rho_out[0],
-                orb_rho_out[1:4],
-                orb_rho_out[4],
+                orb_rho_out,
                 apply_transformation=False,
             )
             occd_feats.append(occd_feat)
@@ -212,13 +197,9 @@ class LCAONLDFGenerator:
         vf_gq = np.zeros((vfeat.shape[1], self.interpolator.num_out))
         vf_gq = self.plan.eval_vxc_full(
             vfeat,
-            vrho_out[0],
-            vrho_out[1:4],
-            vrho_out[4],
+            vrho_out,
             cache["dfeat"],
-            cache["rho_in"][0],
-            cache["rho_in"][1:4],
-            cache["rho_in"][4],
+            cache["rho_in"],
             vf=vf_gq,
             spin=spin,
         )
@@ -240,8 +221,10 @@ class LCAONLDFGenerator:
             * 2
             * cache["rho_in"][1:4]
         )
-        vrho_out[4] += (
-            varg_out_g * cache["darg_in_g"][2] + vfunc_out_g * cache["dfunc_in_g"][2]
-        )
+        if self.plan.nldf_settings.sl_level == "MGGA":
+            vrho_out[4] += (
+                varg_out_g * cache["darg_in_g"][2]
+                + vfunc_out_g * cache["dfunc_in_g"][2]
+            )
         # self._cache[spin] = None
         return vrho_out

ciderpress/dft/plans.py
@@ -16,6 +16,7 @@ from ciderpress.dft.settings import (
     ds2,
     get_alpha,
     get_cider_exponent,
+    get_cider_exponent_gga,
     get_s2,
 )
 from ciderpress.lib import load_library as load_cider_library
@@ -223,9 +224,12 @@ class SemilocalPlan:
 
     def _fill_vxc_nst_(self, rho, vfeat, vxc):
         vxc[:, 0] += self.nspin * vfeat[:, 0]
-        vxc[:, 1:4] += (self.nspin * self.nspin * 2 * vfeat[:, 1])[:, None, :] * rho[
-            :, 1:4
-        ]
+        # fmt: off
+        vxc[:, 1:4] += (
+            (self.nspin * self.nspin * 2 * vfeat[:, 1])[:, None, :]
+            * rho[:, 1:4]
+        )
+        # fmt: on
         vxc[:, 4] += self.nspin * vfeat[:, 2]
 
     def _fill_vxc_np_(self, rho, vfeat, vxc):
@@ -237,9 +241,12 @@ class SemilocalPlan:
 
     def _fill_vxc_ns_(self, rho, vfeat, vxc):
         vxc[:, 0] += self.nspin * vfeat[:, 0]
-        vxc[:, 1:4] += (self.nspin * self.nspin * 2 * vfeat[:, 1])[:, None, :] * rho[
-            :, 1:4
-        ]
+        # fmt: off
+        vxc[:, 1:4] += (
+            (self.nspin * self.nspin * 2 * vfeat[:, 1])[:, None, :]
+            * rho[:, 1:4]
+        )
+        # fmt: on
 
     def get_vxc(self, rho, vfeat, vxc=None):
         assert rho.ndim == 3
@@ -1226,9 +1233,16 @@ class NLDFAuxiliaryPlan(ABC):
     def _get_extra_args(self, i):
         assert 0 <= i < self.nldf_settings.num_feat_param_sets
         params = self.nldf_settings.feat_params[i]
-        return [] if len(params) < 4 else params[3:]
+        # return [] if len(params) < 4 else params[3:]
+        # TODO this works for GGA and MGGA but not if there are
+        # multiple extra args for the feature specs. This might
+        # need to be revised later on.
+        if self.nldf_settings.feat_spec_list[i] == "se_erf_rinv":
+            return [params[-1]]
+        else:
+            return []
 
-    def get_function_to_convolve(self, rho, sigma, tau):
+    def get_function_to_convolve(self, rho_tuple):
         """
         Returns the function that gets convolved to make the nonlocal
         density features. Usually the density rho (settings.rho_mult='one'),
@@ -1241,29 +1255,35 @@ class NLDFAuxiliaryPlan(ABC):
             tau (np.ndarray): Kinetic energy density
 
         Returns:
-            function to convolve, as well as its derivatives with respect
-            to rho, sigma, and tau. Combined, this makes for a tuple of
-            four arrays.
+            tuple(np.ndarray): function to convolve, as well as its derivatives
+            with respect to rho, sigma, and tau. Combined, this makes for a
+            tuple of four arrays.
         """
+        rho = rho_tuple[0]
         if self.nldf_settings.rho_mult == "one":
-            return (
-                rho,
+            da = [
                 np.ones_like(rho),
-                np.zeros_like(rho),
-                np.zeros_like(rho),
-            )
+            ] + [np.zeros_like(r) for r in rho_tuple[1:]]
+            return rho, tuple(da)
         elif self.nldf_settings.rho_mult == "expnt":
-            a, dadn, dads, dadt = self.eval_feat_exp(rho, sigma, tau, i=-1)
-            dadn[:] *= rho
-            dadn[:] += a
-            dads[:] *= rho
-            dadt[:] *= rho
+            a, da_tuple = self.eval_feat_exp(rho_tuple, i=-1)
+            for da in da_tuple:
+                da[:] *= rho
+            da_tuple[0][:] += a
             a[:] *= rho
-            return a, dadn, dads, dadt
+            return a, da_tuple
         else:
             raise NotImplementedError
 
-    def eval_feat_exp(self, rho, sigma, tau, i=-1):
+    def get_rho_tuple(self, rho_data):
+        drho = rho_data[1:4]
+        sigma = np.einsum("xg,xg->g", drho, drho)
+        if self.nldf_settings.sl_level == "MGGA":
+            return rho_data[0], sigma, rho_data[4]
+        else:
+            return rho_data[0], sigma
+
+    def eval_feat_exp(self, rho_tuple, i=-1):
         """
         Evaluate the exponents that determine the length-scale
         of feature i at each grid point. i can take a range
@@ -1279,22 +1299,42 @@ class NLDFAuxiliaryPlan(ABC):
         Returns:
             the cider exponent array (np.ndarray)
         """
-        if i == -1:
-            a0, grad_mul, tau_mul = self.nldf_settings.theta_params
+        if self.nldf_settings.sl_level == "MGGA":
+            rho, sigma, tau = rho_tuple
+            if i == -1:
+                a0, grad_mul, tau_mul = self.nldf_settings.theta_params
+            else:
+                if not 0 <= i < self.nldf_settings.num_feat_param_sets:
+                    raise ValueError("Feature index out of range")
+                a0, grad_mul, tau_mul = self.nldf_settings.feat_params[i][:3]
+            a, dadn, dadsigma, dadtau = get_cider_exponent(
+                rho,
+                sigma,
+                tau,
+                a0=a0,
+                grad_mul=grad_mul,
+                tau_mul=tau_mul,
+                rhocut=self.rhocut,
+                nspin=self.nspin,
+            )
+            return a, (dadn, dadsigma, dadtau)
         else:
-            if not 0 <= i < self.nldf_settings.num_feat_param_sets:
-                raise ValueError("Feature index out of range")
-            a0, grad_mul, tau_mul = self.nldf_settings.feat_params[i][:3]
-        return get_cider_exponent(
-            rho,
-            sigma,
-            tau,
-            a0=a0,
-            grad_mul=grad_mul,
-            tau_mul=tau_mul,
-            rhocut=self.rhocut,
-            nspin=self.nspin,
-        )
+            rho, sigma = rho_tuple
+            if i == -1:
+                a0, grad_mul = self.nldf_settings.theta_params[:2]
+            else:
+                if not 0 <= i < self.nldf_settings.num_feat_param_sets:
+                    raise ValueError("Feature index out of range")
+                a0, grad_mul = self.nldf_settings.feat_params[i][:2]
+            a, dadn, dadsigma = get_cider_exponent_gga(
+                rho,
+                sigma,
+                a0=a0,
+                grad_mul=grad_mul,
+                rhocut=self.rhocut,
+                nspin=self.nspin,
+            )
+            return a, (dadn, dadsigma)
 
     def _clear_l1_cache(self, s):
         self._cached_l1_data[s] = []
@@ -1372,9 +1412,7 @@ class NLDFAuxiliaryPlan(ABC):
     def eval_rho_full(
         self,
         f,
-        rho,
-        drho,
-        tau,
+        rho_data,
         spin=0,
         feat=None,
         dfeat=None,
@@ -1385,7 +1423,7 @@ class NLDFAuxiliaryPlan(ABC):
 
         Args:
             f:
-            rho:
+            rho: [n, dn/dx, dn/dy, dn/dz, (tau)]
             drho:
             tau:
             feat:
@@ -1400,7 +1438,7 @@ class NLDFAuxiliaryPlan(ABC):
         else:
             f_qg = f.T
         ngrids = f_qg.shape[1]
-        sigma = np.einsum("xg,xg->g", drho, drho)
+        rho_tuple = self.get_rho_tuple(rho_data)
         num_vj = self.nldf_settings.num_feat_param_sets
         if feat is None:
             feat = np.empty((self.nldf_settings.nfeat, f_qg.shape[1]))
@@ -1409,9 +1447,7 @@ class NLDFAuxiliaryPlan(ABC):
         self._clear_p_cache(spin)
         dbuf = self.empty_coefs(ngrids, local=False)
         for i in range(num_vj):
-            a_g, dadrho_g, dadsigma_g, dadtau_g = self.get_interpolation_arguments(
-                rho, sigma, tau, i=i
-            )
+            a_g = self.get_interpolation_arguments(rho_tuple, i=i)[0]
             p, dp = self.get_interpolation_coefficients(a_g, i=i, dbuf=dbuf)
             if apply_transformation:
                 self.get_transformed_interpolation_terms(p, i=i, fwd=True, inplace=True)
@@ -1425,7 +1461,7 @@ class NLDFAuxiliaryPlan(ABC):
         start = 0 if self.nldf_settings.nldf_type == "i" else self.nalpha
         self._clear_l1_cache(spin)
         if "i" in self.nldf_settings.nldf_type:
-            self.eval_rho_vi_(f_qg[start:], drho, feat[num_vj:], spin=spin)
+            self.eval_rho_vi_(f_qg[start:], rho_data[1:4], feat[num_vj:], spin=spin)
         feat[:] *= self.nspin
         # dfeat[:] *= self.nspin
         return feat, dfeat
@@ -1433,13 +1469,9 @@ class NLDFAuxiliaryPlan(ABC):
     def eval_occd_full(
         self,
         f,
-        rho,
-        drho,
-        tau,
+        rho_data,
         occd_f,
-        occd_rho,
-        occd_drho,
-        occd_tau,
+        occd_rho_data,
         apply_transformation=True,
     ):
         spin = 0
@@ -1452,20 +1484,19 @@ class NLDFAuxiliaryPlan(ABC):
             f_qg = f.T
             occd_f_qg = occd_f.T
         ngrids = f_qg.shape[1]
-        sigma = np.einsum("xg,xg->g", drho, drho)
-        occd_sigma = 2 * np.einsum("xg,xg->g", drho, occd_drho)
+        rho_tuple = self.get_rho_tuple(rho_data)
+        occd_sigma = 2 * np.einsum("xg,xg->g", rho_data[1:4], occd_rho_data[1:4])
         num_vj = self.nldf_settings.num_feat_param_sets
         occd1_feat = np.zeros((self.nldf_settings.nfeat, f_qg.shape[1]))
         occd2_feat = np.zeros((self.nldf_settings.nfeat, f_qg.shape[1]))
         dbuf = self.empty_coefs(ngrids, local=False)
         for i in range(num_vj):
-            a_g, dadrho_g, dadsigma_g, dadtau_g = self.get_interpolation_arguments(
-                rho, sigma, tau, i=i
-            )
+            a_g, da_tuple = self.get_interpolation_arguments(rho_tuple, i=i)
             p, dp = self.get_interpolation_coefficients(a_g, i=i, dbuf=dbuf)
-            occd_a_g = (
-                occd_rho * dadrho_g + occd_sigma * dadsigma_g + occd_tau * dadtau_g
-            )
+            occd_a_g = occd_rho_data[0] * da_tuple[0]
+            occd_a_g[:] += occd_sigma * da_tuple[1]
+            if self.nldf_settings.sl_level == "MGGA":
+                occd_a_g[:] += occd_rho_data[4] * da_tuple[2]
             if self.coef_order == "qg":
                 dp *= occd_a_g
             else:
@@ -1479,10 +1510,10 @@ class NLDFAuxiliaryPlan(ABC):
             self.eval_rho_vj_(occd_f_qg[: self.nalpha], [p], occd2_feat[i : i + 1])
         start = 0 if self.nldf_settings.nldf_type == "i" else self.nalpha
         self._clear_l1_cache(spin)
-        self._cache_l1_vectors(f_qg[start:], drho, spin)
+        self._cache_l1_vectors(f_qg[start:], rho_data[1:4], spin)
         l1_vals = [v for v in self._cached_l1_data[spin]]
         self._clear_l1_cache(spin)
-        self._cache_l1_vectors(occd_f_qg[start:], occd_drho, spin)
+        self._cache_l1_vectors(occd_f_qg[start:], occd_rho_data[1:4], spin)
         l1_occd = [v for v in self._cached_l1_data[spin]]
         i = 0
         for loc in self._l0_start_locs:
@@ -1498,13 +1529,9 @@ class NLDFAuxiliaryPlan(ABC):
     def eval_vxc_full(
         self,
         vfeat,
-        vrho,
-        vdrho,
-        vtau,
+        vrho_data,
         dfeat,
-        rho,
-        drho,
-        tau,
+        rho_data,
         spin=0,
         p_i_qg=None,
         vf=None,
@@ -1549,21 +1576,20 @@ class NLDFAuxiliaryPlan(ABC):
             start = self.nalpha
             self.eval_vxc_vj_(vfeat[:num_vj], p_i_qg, vf_qg[:start])
         if "i" in self.nldf_settings.nldf_type:
-            self.eval_vxc_vi_(vfeat[num_vj:], vdrho, vf_qg[start:], spin)
+            self.eval_vxc_vi_(vfeat[num_vj:], vrho_data[1:4], vf_qg[start:], spin)
         num_vj = self.nldf_settings.num_feat_param_sets
-        sigma = np.einsum("xg,xg->g", drho, drho)
+        rho_tuple = self.get_rho_tuple(rho_data)
         for i in range(num_vj):
-            a_g, dadrho_g, dadsigma_g, dadtau_g = self.get_interpolation_arguments(
-                rho, sigma, tau, i=i
-            )
-            vrho[:] += dadrho_g * dfeat[i] * vfeat[i]
-            vdrho[:] += 2 * drho * dadsigma_g * dfeat[i] * vfeat[i]
-            vtau[:] += dadtau_g * dfeat[i] * vfeat[i]
+            da_tuple = self.get_interpolation_arguments(rho_tuple, i=i)[1]
+            vrho_data[0, :] += da_tuple[0] * dfeat[i] * vfeat[i]
+            vrho_data[1:4, :] += 2 * rho_data[1:4] * da_tuple[1] * dfeat[i] * vfeat[i]
+            if self.nldf_settings.sl_level == "MGGA":
+                vrho_data[4, :] += da_tuple[2] * dfeat[i] * vfeat[i]
         # self._clear_p_cache(spin)
         return vf_qg if self.coef_order == "qg" else vf_qg.T
 
     @abstractmethod
-    def _get_interpolation_arguments(self, rho, sigma, tau, i=-1):
+    def _get_interpolation_arguments(self, rho_tuple, i=-1):
         pass
 
     @abstractmethod
@@ -1574,11 +1600,11 @@ class NLDFAuxiliaryPlan(ABC):
     def _get_transformed_interpolation_terms(self, p_xx, i=-1, fwd=True, inplace=False):
         pass
 
-    def get_interpolation_arguments(self, rho, sigma, tau, i=-1):
+    def get_interpolation_arguments(self, rho_tuple, i=-1):
         if i == -1 and self.nldf_settings.nldf_type == "k":
-            return self.eval_feat_exp(rho, sigma, tau, i=i)
+            return self.eval_feat_exp(rho_tuple, i=i)
         else:
-            return self._get_interpolation_arguments(rho, sigma, tau, i=i)
+            return self._get_interpolation_arguments(rho_tuple, i=i)
 
     def get_interpolation_coefficients(self, arg_g, i=-1, vbuf=None, dbuf=None):
         if not (-1 <= i < self.nldf_settings.num_feat_param_sets):
@@ -1655,11 +1681,15 @@ class NLDFGaussianPlan(NLDFAuxiliaryPlan):
             self, self.alphas, i=-1, local=False
         )
         ovlp *= self.alpha_norms[None, :] * self.alpha_norms[:, None]
-        ref = np.identity(ovlp.shape[0]) * self.alpha_norms[:, None]
-        self._alpha_transform = _stable_solve(ovlp, ref)
+        self._dmul = False
+        if self._dmul:
+            ref = np.identity(ovlp.shape[0]) * self.alpha_norms[:, None]
+            self._alpha_transform = _stable_solve(ovlp, ref)
+        else:
+            self._alpha_transform = ovlp
 
-    def _get_interpolation_arguments(self, rho, sigma, tau, i=-1):
-        return self.eval_feat_exp(rho, sigma, tau, i=i)
+    def _get_interpolation_arguments(self, rho_tuple, i=-1):
+        return self.eval_feat_exp(rho_tuple, i=i)
 
     def _get_interpolation_coefficients(
         self, arg_g, i=-1, local=True, vbuf=None, dbuf=None
@@ -1693,11 +1723,25 @@ class NLDFGaussianPlan(NLDFAuxiliaryPlan):
             p_qu = p_xx.transpose()
         else:
             p_qu = p_xx
-        # TODO parallelize with openmp
-        if inplace:
-            p_qu[:] = transform.dot(p_qu)
+        if self._dmul:
+            # TODO parallelize with openmp
+            if inplace:
+                p_qu[:] = transform.dot(p_qu)
+            else:
+                p_qu = transform.dot(p_qu)
         else:
-            p_qu = transform.dot(p_qu)
+            if inplace:
+                if fwd:
+                    p_qu[:] *= self.alpha_norms[:, None]
+                p_qu[:] = _stable_solve(transform, p_qu)
+                if not fwd:
+                    p_qu[:] *= self.alpha_norms[:, None]
+            else:
+                if fwd:
+                    p_qu = self.alpha_norms[:, None] * p_qu
+                p_qu = _stable_solve(transform, p_qu)
+                if not fwd:
+                    p_qu = self.alpha_norms[:, None] * p_qu
         if self.coef_order == "gq":
             p_xx = p_qu.T
         else:
@@ -1804,13 +1848,12 @@ class NLDFSplinePlan(NLDFAuxiliaryPlan):
             derivi[:] *= (self._spline_size - 1) / (self.nalpha - 1)
         return di, derivi
 
-    def _get_interpolation_arguments(self, rho, sigma, tau, i=-1):
-        a_g, dadn_g, dadsigma_g, dadtau_g = self.eval_feat_exp(rho, sigma, tau, i=i)
+    def _get_interpolation_arguments(self, rho_tuple, i=-1):
+        a_g, da_tuple = self.eval_feat_exp(rho_tuple, i=i)
         di, derivi = self.get_a2q_fast(a_g)
-        dadn_g[:] *= derivi
-        dadsigma_g[:] *= derivi
-        dadtau_g[:] *= derivi
-        return di, dadn_g, dadsigma_g, dadtau_g
+        for da in da_tuple:
+            da[:] *= derivi
+        return di, da_tuple
 
     def _get_interpolation_coefficients(
         self, arg_g, i=-1, local=True, vbuf=None, dbuf=None

ciderpress/dft/settings.py
@@ -38,18 +38,14 @@ def _get_fl_ueg(s):
         return 0.75
     elif np.abs(s - 1.0) < tol:
         return 0.6
+    # fmt: off
     return (
-        sign
-        * 4
-        * np.pi
-        * 3
-        * np.cos(np.pi * s)
-        * gamma_func(-1 - 2 * s)
-        / (3 + 2 * s)
-        * 4**s
+        sign * 4 * np.pi * 3 * np.cos(np.pi * s)
+        * gamma_func(-1 - 2 * s) / (3 + 2 * s) * 4**s
         * gamma_func(1.5 + s)
         / (np.pi**1.5 * np.abs(gamma_func(-s)))
     )
+    # fmt: on
 
 
 def get_cider_exponent(
@@ -93,6 +89,41 @@ def get_cider_exponent(
         dadsigma = dadsigma.item()
         dadtau = dadtau.item()
     return ascale, dadrho, dadsigma, dadtau
+
+
+def get_cider_exponent_gga(rho, sigma, a0=1.0, grad_mul=0.03125, rhocut=1e-10, nspin=1):
+    if nspin > 2:
+        raise ValueError
+    if isinstance(rho, np.ndarray):
+        isarray = True
+    else:
+        isarray = False
+        rho = np.asarray([rho], dtype=np.float64)
+        sigma = np.asarray([sigma], dtype=np.float64)
+    cond = rho < rhocut
+    rho = rho.copy()
+    rho[cond] = rhocut
+    if nspin == 1:
+        B = np.pi / 2 ** (2.0 / 3) * a0
+    else:
+        B = np.pi * a0
+    ascale = B * rho ** (2.0 / 3)
+    dadrho = 2 * B / (3 * rho ** (1.0 / 3))
+    if grad_mul != 0:
+        grad_fac = grad_mul * 1.2 * (6 * np.pi**2) ** (2.0 / 3) / np.pi
+        grad_fac *= np.pi / 2 ** (2.0 / 3) * 0.125 / CFC
+        ascale += grad_fac * sigma / (rho * rho)
+        dadsigma = grad_fac / (rho * rho)
+        dadrho -= 2 * grad_fac * sigma / (rho * rho * rho)
+    else:
+        dadsigma = np.zeros_like(ascale)
+    dadrho[cond] = 0
+    dadsigma[cond] = 0
+    if not isarray:
+        ascale = ascale.item()
+        dadrho = dadrho.item()
+        dadsigma = dadsigma.item()
+    return ascale, dadrho, dadsigma
 
 
 def _get_ueg_expnt(aval, tval, rho):
@@ -919,6 +950,7 @@ class NLDFSettings(BaseSettings):
 
     def __init__(
         self,
+        sl_level,
         theta_params,
         rho_mult,
     ):
@@ -926,15 +958,19 @@ class NLDFSettings(BaseSettings):
         Initialize NLDFSettings.
 
         Args:
-            theta_params (np.ndarray(3)):
+            sl_level (str): "GGA" or "MGGA", the level of semilocal ingredients
+                used to construct the length-scale exponent.
+            theta_params (np.ndarray(2 or 3)):
                 Settings for the squared-exponential kernel exponent for the r'
                 (integrated) coordinate of the features. For version 'k', this
                 'exponent' is not used in the squared-exponential but rather
                 within the density damping scheme (see rho_damp).
                 Should be an array of 3 floats [a0, grad_mul, tau_mul].
+                tau_mul is ignored if sl_level="GGA" and may therefore be excluded.
             rho_mult (str): Multiply the density that gets integrated
                 by a prefactor. Options: None/'one', 'taumix', 'dampmix', 'expnt'
         """
+        self._sl_level = sl_level
         self.theta_params = theta_params
         self.rho_mult = rho_mult
         self.l0_feat_specs = []
@@ -942,8 +978,18 @@ class NLDFSettings(BaseSettings):
         self.l1_feat_dots = []
         self.feat_params = []
         self._check_params(self.theta_params)
+        if self.sl_level not in ["GGA", "MGGA"]:
+            raise ValueError("sl_level must be GGA or MGGA")
         if self.rho_mult not in ALLOWED_RHO_MULTS:
             raise ValueError("Unsupported rho_mult")
+
+    @property
+    def sl_level(self):
+        # backward compatibility
+        if hasattr(self, "_sl_level"):
+            return self._sl_level
+        else:
+            return "MGGA"
 
     @staticmethod
     def _check_params(params, spec="se"):
@@ -1016,6 +1062,7 @@ class NLDFSettingsVI(NLDFSettings):
 
     def __init__(
         self,
+        sl_level,
         theta_params,
         rho_mult,
         l0_feat_specs,
@@ -1026,6 +1073,8 @@ class NLDFSettingsVI(NLDFSettings):
         Initialize NLDFSettingsVI object
 
         Args:
+            sl_level (str): "GGA" or "MGGA", the level of semilocal ingredients
+                used to construct the length-scale exponent.
             theta_params (np.ndarray):
                 Settings for the squared-exponential kernel exponent for the r'
                 (integrated) coordinate of the features. For version 'k', this
@@ -1046,7 +1095,7 @@ class NLDFSettingsVI(NLDFSettings):
                 -1 refers to the semilocal density gradient.
                 l1feat_ig = einsum('xg,xg->g', l1ints_jg, l1ints_kg)
         """
-        super(NLDFSettingsVI, self).__init__(theta_params, rho_mult)
+        super(NLDFSettingsVI, self).__init__(sl_level, theta_params, rho_mult)
         self.l0_feat_specs = l0_feat_specs
         self.l1_feat_specs = l1_feat_specs
         self.l1_feat_dots = l1_feat_dots
@@ -1142,6 +1191,7 @@ class NLDFSettingsVJ(NLDFSettings):
 
     def __init__(
         self,
+        sl_level,
         theta_params,
         rho_mult,
         feat_specs,
@@ -1151,6 +1201,8 @@ class NLDFSettingsVJ(NLDFSettings):
         Initialize NLDFSettingsVJ
 
         Args:
+            sl_level (str): "GGA" or "MGGA", the level of semilocal ingredients
+                used to construct the length-scale exponent.
             theta_params (np.ndarray):
                 Settings for the squared-exponential kernel exponent for the r'
                 (integrated) coordinate of the features. For version 'k', this
@@ -1169,8 +1221,9 @@ class NLDFSettingsVJ(NLDFSettings):
                 has three numbers [a0, grad_mul, tau_mul], except erf_rinv which
                 has an additional parameter erf_mul for the ratio of the
                 erf / rinv exponent to the squared-exponential exponent.
+                tau_mul is ignored if sl_level="GGA" and may therefore be excluded.
         """
-        super(NLDFSettingsVJ, self).__init__(theta_params, rho_mult)
+        super(NLDFSettingsVJ, self).__init__(sl_level, theta_params, rho_mult)
         self.feat_params = feat_params
         self.feat_specs = feat_specs
         self._check_specs(self.feat_specs, ALLOWED_J_SPECS)
@@ -1254,6 +1307,7 @@ class NLDFSettingsVIJ(NLDFSettings):
 
     def __init__(
         self,
+        sl_level,
         theta_params,
         rho_mult,
         l0_feat_specs_i,
@@ -1266,6 +1320,8 @@ class NLDFSettingsVIJ(NLDFSettings):
         Initialize NLDFSettingsVIJ object
 
         Args:
+            sl_level (str): "GGA" or "MGGA", the level of semilocal ingredients
+                used to construct the length-scale exponent.
             theta_params (np.ndarray):
                 Settings for the squared-exponential kernel exponent for the r'
                 (integrated) coordinate of the features. For version 'k', this
@@ -1296,8 +1352,9 @@ class NLDFSettingsVIJ(NLDFSettings):
                 has three numbers [a0, grad_mul, tau_mul], except erf_rinv which
                 has an additional parameter erf_mul for the ratio of the
                 erf / rinv exponent to the squared-exponential exponent.
+                tau_mul is ignored if sl_level="GGA" and may therefore be excluded.
         """
-        super(NLDFSettingsVIJ, self).__init__(theta_params, rho_mult)
+        super(NLDFSettingsVIJ, self).__init__(sl_level, theta_params, rho_mult)
         self.l0_feat_specs = l0_feat_specs_i
         self.l1_feat_specs = l1_feat_specs_i
         self.l1_feat_dots = l1_feat_dots_i
@@ -1365,6 +1422,7 @@ class NLDFSettingsVK(NLDFSettings):
 
     def __init__(
         self,
+        sl_level,
         theta_params,
         rho_mult,
         feat_params,
@@ -1374,6 +1432,8 @@ class NLDFSettingsVK(NLDFSettings):
         Initialize NLDFSettingsVK
 
         Args:
+            sl_level (str): "GGA" or "MGGA", the level of semilocal ingredients
+                used to construct the length-scale exponent.
             theta_params (np.ndarray):
                 Settings for the squared-exponential kernel exponent for the r'
                 (integrated) coordinate of the features. For version 'k', this
@@ -1387,10 +1447,11 @@ class NLDFSettingsVK(NLDFSettings):
                 has three numbers [a0, grad_mul, tau_mul], except erf_rinv which
                 has an additional parameter erf_mul for the ratio of the
                 erf / rinv exponent to the squared-exponential exponent.
+                tau_mul is ignored if sl_level="GGA" and may therefore be excluded.
             rho_damp (str): Specifies the damping for the
                 density. Options: 'none', 'exponential', 'asymptotic_const'
         """
-        super(NLDFSettingsVK, self).__init__(theta_params, rho_mult)
+        super(NLDFSettingsVK, self).__init__(sl_level, theta_params, rho_mult)
         self.feat_specs = ["se"] * len(feat_params)
         self.feat_params = feat_params
         self._check_specs(self.feat_specs, ALLOWED_K_SPECS)

ciderpress/dft/tests/test_plans.py
@@ -182,14 +182,22 @@ class TestNLDFGaussianPlan(unittest.TestCase):
         i1specs = ["se_rvec", "se_grad"]
         i1dots = [(-1, 1), (-1, 0), (1, 0)]
         cls.example_vi_settings = NLDFSettingsVI(
+            "MGGA",
             thetap,
             "one",
             i0specs,
             i1specs,
             i1dots,
         )
-        cls.example_vj_settings = NLDFSettingsVJ(thetap, "one", feat_specs, feat_params)
+        cls.example_vj_settings = NLDFSettingsVJ(
+            "MGGA",
+            thetap,
+            "one",
+            feat_specs,
+            feat_params,
+        )
         cls.example_vij_settings = NLDFSettingsVIJ(
+            "MGGA",
             thetap,
             "one",
             i0specs,
@@ -199,6 +207,7 @@ class TestNLDFGaussianPlan(unittest.TestCase):
             feat_params,
         )
         cls.example_vk_settings = NLDFSettingsVK(
+            "MGGA",
             thetap,
             "one",
             feat_params[:2],
@@ -216,8 +225,15 @@ class TestNLDFGaussianPlan(unittest.TestCase):
         sigma = np.einsum("xg,xg->g", self.rho[1:4], self.rho[1:4])
         results = []
         for i in range(5):
-            results.append(plan.eval_feat_exp(self.rho[0], sigma, self.rho[4], i=i))
-        results.append(plan.eval_feat_exp(self.rho[0], sigma, self.rho[4], i=-1))
+            results.append(plan.eval_feat_exp((self.rho[0], sigma, self.rho[4]), i=i))
+        results.append(plan.eval_feat_exp((self.rho[0], sigma, self.rho[4]), i=-1))
+        for i in range(len(results)):
+            results[i] = [
+                results[i][0],
+                results[i][1][0],
+                results[i][1][1],
+                results[i][1][2],
+            ]
         for j in range(4):
             assert_equal(results[0][j], results[3][j])
             assert_equal(results[1][j], results[4][j])
@@ -227,11 +243,11 @@ class TestNLDFGaussianPlan(unittest.TestCase):
             rho_tmp = self.rho[0].copy()
             sigma_tmp = sigma.copy()
             tau_tmp = self.rho[4].copy()
-            a, dadn, dadsigma, dadtau = plan.eval_feat_exp(
-                rho_tmp, sigma_tmp, tau_tmp, i=i
+            a, (dadn, dadsigma, dadtau) = plan.eval_feat_exp(
+                (rho_tmp, sigma_tmp, tau_tmp), i=i
             )
-            a2, da2dn, da2dsigma, da2dtau = plan2.eval_feat_exp(
-                0.5 * rho_tmp, 0.25 * sigma_tmp, 0.5 * tau_tmp, i=i
+            a2, (da2dn, da2dsigma, da2dtau) = plan2.eval_feat_exp(
+                (0.5 * rho_tmp, 0.25 * sigma_tmp, 0.5 * tau_tmp), i=i
             )
             assert_almost_equal(a2, a, 12)
             assert_almost_equal(da2dn, 2 * dadn, 12)
@@ -241,16 +257,16 @@ class TestNLDFGaussianPlan(unittest.TestCase):
             derivs = [dadn, dadsigma, dadtau]
             for input, deriv in zip(inputs, derivs):
                 input[:] += 0.5 * DELTA
-                ap = plan.eval_feat_exp(rho_tmp, sigma_tmp, tau_tmp, i=i)[0]
+                ap = plan.eval_feat_exp((rho_tmp, sigma_tmp, tau_tmp), i=i)[0]
                 input[:] -= DELTA
-                am = plan.eval_feat_exp(rho_tmp, sigma_tmp, tau_tmp, i=i)[0]
+                am = plan.eval_feat_exp((rho_tmp, sigma_tmp, tau_tmp), i=i)[0]
                 input[:] += 0.5 * DELTA
                 assert_almost_equal(deriv, (ap - am) / DELTA, 7)
             rho_tiny = 5e-11 * np.ones_like(rho_tmp)
             sigma_tiny = sigma_tmp * rho_tiny**2 / rho_tmp**2
             tau_tiny = tau_tmp * rho_tiny / rho_tmp
-            a, dadn, dadsigma, dadtau = plan.eval_feat_exp(
-                rho_tiny, sigma_tiny, tau_tiny, i=i
+            a, (dadn, dadsigma, dadtau) = plan.eval_feat_exp(
+                (rho_tiny, sigma_tiny, tau_tiny), i=i
             )
             assert (dadn == 0).all()
             assert (dadsigma == 0).all()
@@ -271,33 +287,33 @@ class TestNLDFGaussianPlan(unittest.TestCase):
             rho_tmp = rho.copy()
             sigma_tmp = sigma.copy()
             tau_tmp = tau.copy()
-            a, dadn, dadsigma, dadtau = plan.get_interpolation_arguments(
-                rho_tmp, sigma_tmp, tau_tmp, i=i
+            a, (dadn, dadsigma, dadtau) = plan.get_interpolation_arguments(
+                (rho_tmp, sigma_tmp, tau_tmp), i=i
             )
             inputs = [rho_tmp, sigma_tmp, tau_tmp]
             derivs = [dadn, dadsigma, dadtau]
             for input, deriv in zip(inputs, derivs):
                 input[:] += 0.5 * DELTA
-                ap = plan.get_interpolation_arguments(rho_tmp, sigma_tmp, tau_tmp, i=i)[
-                    0
-                ]
+                ap = plan.get_interpolation_arguments(
+                    (rho_tmp, sigma_tmp, tau_tmp), i=i
+                )[0]
                 input[:] -= DELTA
-                am = plan.get_interpolation_arguments(rho_tmp, sigma_tmp, tau_tmp, i=i)[
-                    0
-                ]
+                am = plan.get_interpolation_arguments(
+                    (rho_tmp, sigma_tmp, tau_tmp), i=i
+                )[0]
                 input[:] += 0.5 * DELTA
                 assert_almost_equal(deriv, (ap - am) / DELTA, 7)
             rho_tiny = 5e-11 * np.ones_like(rho_tmp)
             sigma_tiny = sigma_tmp * rho_tiny**2 / rho_tmp**2
             tau_tiny = tau_tmp * rho_tiny / rho_tmp
-            a, dadn, dadsigma, dadtau = plan.eval_feat_exp(
-                rho_tiny, sigma_tiny, tau_tiny, i=i
+            a, (dadn, dadsigma, dadtau) = plan.eval_feat_exp(
+                (rho_tiny, sigma_tiny, tau_tiny), i=i
             )
             assert (dadn == 0).all()
             assert (dadsigma == 0).all()
             assert (dadtau == 0).all()
         try:
-            plan.get_interpolation_arguments(rho, sigma, tau, i=5)
+            plan.get_interpolation_arguments((rho, sigma, tau), i=5)
             raise AssertionError
         except ValueError:
             pass
@@ -310,8 +326,8 @@ class TestNLDFGaussianPlan(unittest.TestCase):
         feat_parts = self._get_feat(plan.alphas[:, None], "se", None)
         feat_parts *= plan.alpha_norms[:, None]
         for i in range(-1, 5):
-            exp_g = plan.eval_feat_exp(rho, sigma, tau, i=i)[0]
-            arg_g = plan.get_interpolation_arguments(rho, sigma, tau, i=i)[0]
+            exp_g = plan.eval_feat_exp((rho, sigma, tau), i=i)[0]
+            arg_g = plan.get_interpolation_arguments((rho, sigma, tau), i=i)[0]
             p, dp = plan.get_interpolation_coefficients(arg_g, i=i)
             p2, dp2 = np.empty_like(p), np.empty_like(p)
             plan.get_interpolation_coefficients(arg_g, i=i, vbuf=p2, dbuf=dp2)
@@ -379,11 +395,11 @@ class TestNLDFGaussianPlan(unittest.TestCase):
                 start = 0
             if isinstance(settings, (NLDFSettingsVI, NLDFSettingsVIJ)):
                 f[start:] = np.linspace(0.5, 1.5, f.shape[1])
-        feat, dfeat = plan.eval_rho_full(f, rho, drho, tau)
+        feat, dfeat = plan.eval_rho_full(f, self.rho)
         assert feat.shape == (plan.nldf_settings.nfeat, rho.size)
         assert dfeat.shape == (plan.nldf_settings.num_feat_param_sets, rho.size)
         for i in range(plan.nldf_settings.num_feat_param_sets):
-            exp_g = plan.eval_feat_exp(rho, sigma, tau, i=i)[0]
+            exp_g = plan.eval_feat_exp((rho, sigma, tau), i=i)[0]
             feat_id = plan.nldf_settings.feat_spec_list[i]
             extra_args = plan._get_extra_args(i) if i > -1 else None
             ref_feat = self._get_feat(exp_g, feat_id, extra_args)
@@ -394,10 +410,11 @@ class TestNLDFGaussianPlan(unittest.TestCase):
             return (energy, feat / energy)
 
         vfeat = get_e_and_vfeat(feat)[1]
-        vrho = np.zeros_like(rho)
-        vdrho = np.zeros_like(drho)
-        vtau = np.zeros_like(tau)
-        plan.eval_vxc_full(vfeat, vrho, vdrho, vtau, dfeat, rho, drho, tau, vf=vf)
+        vrho_data = np.zeros_like(self.rho)
+        vrho = vrho_data[0]
+        vdrho = vrho_data[1:4]
+        vtau = vrho_data[4]
+        plan.eval_vxc_full(vfeat, vrho_data, dfeat, self.rho, vf=vf)
         inputs = [rho, drho[0], drho[1], drho[2], tau]
         derivs = [vrho, vdrho[0], vdrho[1], vdrho[2], vtau]
         if coef_order == "gq":
@@ -410,10 +427,10 @@ class TestNLDFGaussianPlan(unittest.TestCase):
         for target, deriv in zip(inputs, derivs):
             i += 1
             target[:] += 0.5 * DELTA
-            feat_tmp, _ = plan.eval_rho_full(f, rho, drho, tau)
+            feat_tmp, _ = plan.eval_rho_full(f, self.rho)
             ep, _ = get_e_and_vfeat(feat_tmp)
             target[:] -= DELTA
-            feat_tmp, _ = plan.eval_rho_full(f, rho, drho, tau)
+            feat_tmp, _ = plan.eval_rho_full(f, self.rho)
             em, _ = get_e_and_vfeat(feat_tmp)
             target[:] += 0.5 * DELTA
             assert_almost_equal(deriv, (ep - em) / DELTA, 6)
@@ -433,11 +450,11 @@ class TestNLDFGaussianPlan(unittest.TestCase):
         rho = self.rho[0]
         sigma = np.einsum("xg,xg->g", self.rho[1:4], self.rho[1:4])
         tau = self.rho[4]
-        exp_g = plan.eval_feat_exp(rho, sigma, tau, i=0)[0]
+        exp_g = plan.eval_feat_exp((rho, sigma, tau), i=0)[0]
         # make some other exponent for testing
         ref_exp_g = exp_g / (0.4 + exp_g) + 0.5 * exp_g
         # arg1_g = plan.get_interpolation_arguments(rho, sigma, tau, i=-1)[0]
-        arg2_g = plan.get_interpolation_arguments(rho, sigma, tau, i=0)[0]
+        arg2_g = plan.get_interpolation_arguments((rho, sigma, tau), i=0)[0]
         p1, dp1 = plan.get_interpolation_coefficients(ref_exp_g, i=-1)
         p2, dp2 = plan.get_interpolation_coefficients(arg2_g, i=0)
         plan.get_transformed_interpolation_terms(p2, i=0, fwd=True, inplace=True)

ciderpress/dft/tests/test_ueg.py
@@ -59,6 +59,7 @@ class TestUEGVector(unittest.TestCase):
         feat_params[3].append(2.0)
 
         isettings = NLDFSettingsVI(
+            "MGGA",
             theta_params,
             "one",
             l0_feat_specs=ALLOWED_I_SPECS_L0,
@@ -66,12 +67,14 @@ class TestUEGVector(unittest.TestCase):
             l1_feat_dots=[(-1, 0), (-1, 1), (0, 1)],
         )
         jsettings = NLDFSettingsVJ(
+            "MGGA",
             theta_params,
             "one",
             feat_specs=ALLOWED_J_SPECS,
             feat_params=feat_params,
         )
         ijsettings = NLDFSettingsVIJ(
+            "MGGA",
             theta_params,
             "one",
             l0_feat_specs_i=ALLOWED_I_SPECS_L0,
@@ -81,7 +84,11 @@ class TestUEGVector(unittest.TestCase):
             feat_params_j=feat_params,
         )
         ksettings = NLDFSettingsVK(
-            theta_params, "one", feat_params=feat_params_k, rho_damp="exponential"
+            "MGGA",
+            theta_params,
+            "one",
+            feat_params=feat_params_k,
+            rho_damp="exponential",
         )
 
         zero = np.zeros((1, 3))

ciderpress/pyscf/numint.py
@@ -79,7 +79,7 @@ def nr_rks(
     if relativity != 0:
         raise NotImplementedError
 
-    xctype = "MGGA"
+    xctype = ni.settings.sl_settings.level
     ni.initialize_feature_generators(mol, grids, 1)
 
     make_rho, nset, nao = ni._gen_rho_evaluator(mol, dms, hermi, False, grids)
@@ -182,7 +182,7 @@ def nr_uks(
     if relativity != 0:
         raise NotImplementedError
 
-    xctype = "MGGA"
+    xctype = ni.settings.sl_settings.level
     ni.initialize_feature_generators(mol, grids, 2)
 
     dma, dmb = numint._format_uks_dm(dms)
@@ -323,7 +323,7 @@ def nr_rks_nldf(
     if relativity != 0:
         raise NotImplementedError
 
-    xctype = "MGGA"
+    xctype = ni.settings.sl_settings.level
     ni.initialize_feature_generators(mol, grids, 1)
     assert ni.has_nldf
 
@@ -338,7 +338,7 @@ def nr_rks_nldf(
     nelec = np.zeros(nset)
     excsum = np.zeros(nset)
     vmat = np.zeros((nset, nao, nao))
-    nrho = 5
+    nrho = 5 if xctype == "MGGA" else 4
     if not ni.settings.nlof_settings.is_empty:
         nrho += ni.settings.nlof_settings.nrho
     rho_full = np.zeros((nset, nrho, grids.weights.size), dtype=np.float64, order="C")
@@ -389,7 +389,6 @@ def nr_rks_nldf(
                     )
                 else:
                     sdmx_feat = None
-                print((nldf_feat[idm] * weight).sum(axis=-1))
                 exc, (vxc, vxc_nldf, vxc_sdmx) = ni.eval_xc_cider(
                     xc_code,
                     rho,
@@ -448,7 +447,7 @@ def nr_uks_nldf(
     if relativity != 0:
         raise NotImplementedError
 
-    xctype = "MGGA"
+    xctype = ni.settings.sl_settings.level
     ni.initialize_feature_generators(mol, grids, 2)
 
     dma, dmb = numint._format_uks_dm(dms)
@@ -472,7 +471,7 @@ def nr_uks_nldf(
     nelec = np.zeros((2, nset))
     excsum = np.zeros(nset)
     vmat = np.zeros((2, nset, nao, nao))
-    nrho = 5
+    nrho = 5 if xctype == "MGGA" else 4
     if not ni.settings.nlof_settings.is_empty:
         nrho += ni.settings.nlof_settings.nrho
     rhoa_full = np.zeros((nset, nrho, grids.weights.size), dtype=np.float64, order="C")
@@ -853,14 +852,15 @@ class CiderNumInt(CiderNumIntMixin, numint.NumInt):
         vmat, v1 = vmats
         aow = buffers
         wv[0] *= 0.5
-        wv[4] *= 0.5
         aow = _scale_ao_sparse(ao[:4], wv[:4], mask, ao_loc, out=aow)
         vmat = _dot_ao_ao_sparse(
             ao[0], aow, None, nbins, mask, pair_mask, ao_loc, hermi=0, out=vmat
         )
-        v1 = _tau_dot_sparse(
-            ao, ao, wv[4], nbins, mask, pair_mask, ao_loc, out=v1, aow=aow
-        )
+        if len(wv) > 4:
+            wv[4] *= 0.5
+            v1 = _tau_dot_sparse(
+                ao, ao, wv[4], nbins, mask, pair_mask, ao_loc, out=v1, aow=aow
+            )
         return (vmat, v1), aow
 
     def block_loop(