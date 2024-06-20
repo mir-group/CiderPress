50d8dcedc6bfb6c09a19f92d532feee78d3712d3
kylebystrom <kylebystrom@gmail.com>
2024-06-20 13:49:51 -0400

    update flake and install gpaw data

--- .github/workflows/setup_gpaw.sh
+++ .github/workflows/setup_gpaw.sh
@@ -5,4 +5,7 @@ cd gpaw
 cp $CIDERDIR/.github/workflows/gpaw_siteconfig.py .
 python setup.py build
 python setup.py install
+cd ..
+gpaw install-data --sg15 $PWD
+gpaw install-data $PWD
 cd $CIDERDIR
