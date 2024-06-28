fix normalization of convolutions; gga-level stress not working

ciderpress/gpaw/cider_fft.py
@@ -685,6 +685,13 @@ class _CiderBase:
         self.kbuf_ak.lock()
         self.theta_ak.lock()
 
+    def get_interpolation_coefficients(self, arg_g, i=-1):
+        p_qg, dp_qg = self._plan.get_interpolation_coefficients(arg_g, i=i)
+        norms = self._plan.alpha_norms[self._plan.proc_inds][:, None]
+        p_qg[:] *= norms
+        dp_qg[:] *= norms
+        return p_qg, dp_qg
+
     def _setup_plan(self):
         nldf_settings = self.cider_kernel.mlfunc.settings.nldf_settings
         need_plan = self._plan is None or self._plan.nspin != self.nspin
@@ -696,7 +703,7 @@ class _CiderBase:
                 self.encut / self.lambd ** (self.Nalpha - 1),
                 self.lambd,
                 self.Nalpha,
-                coef_order="gq",
+                coef_order="qg",
                 alpha_formula="etb",
                 proc_inds=self.alphas,
             )
@@ -766,9 +773,7 @@ class _CiderBase:
                 F_k = np.zeros(self.k2_k.shape, complex)
             for b in self._plan.proc_inds:
                 aexp = self._plan.alphas[a] + self._plan.alphas[b]
-                fac = (
-                    np.pi / aexp
-                ) ** 1.5  # * self._plan.alpha_norms[a] * self._plan.alpha_norms[b]
+                fac = (np.pi / aexp) ** 1.5
                 pwutil.mulexp(
                     F_k.ravel(),
                     self.theta_ak[b].ravel(),
@@ -815,9 +820,7 @@ class _CiderBase:
             for b in self._plan.proc_inds:
                 aexp = self._plan.alphas[a] + self._plan.alphas[b]
                 invexp = 1.0 / (4 * aexp)
-                fac = (
-                    -1 * (np.pi / aexp) ** 1.5 * invexp
-                )  # * self._plan.alpha_norms[a] * self._plan.alpha_norms[b]
+                fac = -1 * (np.pi / aexp) ** 1.5 * invexp
                 pwutil.mulexp(
                     F_k.ravel(),
                     self.theta_ak[b].ravel(),
@@ -876,9 +879,7 @@ class _CiderBase:
             for a in self.alphas:
                 self.rbuf_ag[a][:] = 0.0
         self.timer.start("COEFS")
-        p_qg, dp_qg = self._plan.get_interpolation_coefficients(di.ravel(), i=-1)
-        p_qg = p_qg.T
-        dp_qg = dp_qg.T
+        p_qg, dp_qg = self.get_interpolation_coefficients(di.ravel(), i=-1)
         for ind, a in enumerate(self.alphas):
             q_ag[a] = p_qg[ind]
             dq_ag[a] = dp_qg[ind]
@@ -902,9 +903,7 @@ class _CiderBase:
                 di = cider_exp[i]
             else:
                 di = None
-            p_qg, dp_qg = self._plan.get_interpolation_coefficients(di.ravel(), i=i)
-            p_qg = p_qg.T
-            dp_qg = dp_qg.T
+            p_qg, dp_qg = self.get_interpolation_coefficients(di.ravel(), i=i)
             p_qg.shape = (len(self.alphas),) + di.shape
             dp_qg.shape = (len(self.alphas),) + di.shape
             for ind, a in enumerate(self.alphas):

ciderpress/gpaw/cider_paw.py
@@ -462,7 +462,7 @@ class _CiderPASDW_MPRoutines:
                         if has_ofit:
                             pwutil.pasdw_reduce_g(
                                 F_av[a],
-                                ft_xbg[:, ib],
+                                np.ascontiguousarray(ft_xbg[:, ib]),
                                 c_ag[b],
                                 atom_slice.indset,
                             )

ciderpress/gpaw/paw_cider_kernel.py
@@ -90,15 +90,19 @@ class PAWCiderContribUtils:
         y_bsLk = np.zeros(np.array(dx_sLkb.shape)[[3, 0, 1, 2]])
         for b in range(y_bsLk.shape[0]):
             aexp = self._plan.alphas[b] + self._plan.alphas
-            fac = (
-                np.pi / aexp
-            ) ** 1.5  # * self._plan.alpha_norms * self._plan.alpha_norms[b]
+            fac = (np.pi / aexp) ** 1.5
             y_bsLk[b, :, :, :] += np.einsum(
                 "kq,sLkq->sLk",
                 np.exp(-ks[:, None] ** 2 / (4 * aexp)) * fac,
                 dx_sLkb,
             )
         return y_bsLk.transpose(1, 0, 2, 3)
+
+    def get_interpolation_coefficients(self, arg_g, i=-1):
+        p_qg, dp_qg = self._plan.get_interpolation_coefficients(arg_g, i=i)
+        p_qg[:] *= self._plan.alpha_norms[:, None]
+        dp_qg[:] *= self._plan.alpha_norms[:, None]
+        return p_qg, dp_qg
 
     def get_paw_atom_contribs(self, n_sg, sigma_xg, tau_sg=None, ae=True):
         nspin = n_sg.shape[0]
@@ -112,9 +116,7 @@ class PAWCiderContribUtils:
         # TODO need to account for dfunc
         func_sg, dfunc = self._plan.get_function_to_convolve(rho_tuple)
         for s in range(nspin):
-            p_qg, dp_qg = self._plan.get_interpolation_coefficients(
-                di_s[s].ravel(), i=-1
-            )
+            p_qg, dp_qg = self.get_interpolation_coefficients(di_s[s].ravel(), i=-1)
             for a in range(self.Nalpha):
                 x_sag[s, a] = p_qg[a] * func_sg[s]
                 xd_sag[s, a] = dp_qg[a] * func_sg[s]
@@ -156,9 +158,7 @@ class PAWCiderContribUtils:
         for i in range(nfeat):
             di_s, derivs = self._plan.get_interpolation_arguments(rho_tuple, i=i)
             for s in range(nspin):
-                p_qg, dp_qg = self._plan.get_interpolation_coefficients(
-                    di_s[s].ravel(), i=i
-                )
+                p_qg, dp_qg = self.get_interpolation_coefficients(di_s[s].ravel(), i=i)
                 for a in range(self.Nalpha):
                     x_sig[s, i] += p_qg[a] * y_sbg[s, a]
                     xd_sig[s, i] += dp_qg[a] * y_sbg[s, a]
@@ -241,9 +241,7 @@ class PAWCiderContribUtils:
         else:
             dedtau_sg = None
         for s in range(nspin):
-            p_qg, dp_qg = self._plan.get_interpolation_coefficients(
-                di_s[s].ravel(), i=-1
-            )
+            p_qg, dp_qg = self.get_interpolation_coefficients(di_s[s].ravel(), i=-1)
             for a in range(self.Nalpha):
                 dedn_sg[s] += p_qg[a] * y_sbg[s, a]
                 dedsigma_sg[s] += dp_qg[a] * y_sbg[s, a]