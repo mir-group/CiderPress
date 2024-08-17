2105d3a2c851e26e127aaa740512332073168d70
kylebystrom <kylebystrom@gmail.com>
2024-08-17 14:34:07 -0700

    update functional download script

--- scripts/download_functionals.py
+++ scripts/download_functionals.py
@@ -25,20 +25,11 @@ basedir = __file__
 savedir = os.path.basename(os.path.join(basedir, "../functionals"))
 
 os.makedirs(savedir, exist_ok=True)
-links = {
-    "CIDER23X_NL_GGA": "16IF81oS3Snqp96KdS43orSmpmNyRKFEs",
-    "CIDER23X_NL_MGGA_DTR": "1njP58rnZL0L523hEiwXdQ9YoBTlp0Uev",
-    "CIDER23X_NL_MGGA_PBE": "1sVofPzXu4eN2N0cDSC0wawnvNjqCoXhi",
-    "CIDER23X_NL_MGGA": "1Y_pGWGnuzH7PiTT1UntnCga4NPIXBh0m",
-    "CIDER23X_SL_GGA": "1laNkInAIq1EfHACfOf5hUpqL7eWFpoA1",
-    "CIDER23X_SL_MGGA": "1kmYuG50OhTCNB8QVIEHLm-cEQ8mMjwT8",
-    "CIDER24Xe": "1tFKAMvfYJcUZEz11uFSl-DXGW-47l9SV",
-    "CIDER24Xne": "1OUcWv18e2vM0eSDyiSl1dm9g3A7Wu5_3",
-}
 
 os.chdir(savedir)
-cmd = "wget --no-check-certificate 'https://docs.google.com/uc?export=download&id={}' -O {}"
-for name, linkid in links.items():
-    fname = "{}.yaml".format(name)
-    mycmd = cmd.format(linkid, fname)
-    subprocess.call(mycmd, shell=True)
+cmd = "wget 'https://zenodo.org/api/records/13336814/files-archive' -O cider_functionals.zip"
+unzip = "unzip cider_functionals.zip"
+rm = "rm -r cider_functionals.zip"
+subprocess.call(cmd, shell=True)
+subprocess.call(unzip, shell=True)
+subprocess.call(rm, shell=True)
