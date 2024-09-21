c3b0a98aeb7b7f3d59760153d31f8428fe2e0087
kylebystrom <kylebystrom@gmail.com>
2024-09-21 19:39:04 -0400

    fix url syntax

--- .github/workflows/mm_install.sh
+++ .github/workflows/mm_install.sh
@@ -3,4 +3,4 @@ micromamba install openmpi openmpi-mpicc
 micromamba install libxc conda-forge::fftw
 micromamba install pytorch pytorch-cuda=11.8 -c pytorch -c nvidia
 # TODO below can be removed once next PySCF released
-pip install git+https://github.com/pyscf/pyscf/commit/943d39a970a3c30ce6cd73ac3c5bf386155d388e
+pip install git+https://github.com/pyscf/pyscf.git@943d39a970a3c30ce6cd73ac3c5bf386155d388e
