4613a9ac461eea615f06255d364976a5a35aee64
kylebystrom <kylebystrom@gmail.com>
2024-09-21 19:16:47 -0400

    try to update build environment to CI works

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -3,5 +3,6 @@ export OMP_NUM_THREADS=2
 export PYTHONPATH=$(pwd):$PYTHONPATH
 ulimit -s 20000
 
+# See https://github.com/pytest-dev/pytest/issues/1075
 version=$(python -c 'import sys; print("{0}.{1}".format(*sys.version_info[:2]))')
 PYTHONHASHSEED=0 pytest ciderpress/ -s -c pytest.ini ciderpress
