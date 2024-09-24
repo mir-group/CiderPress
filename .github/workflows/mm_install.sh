97b706785bbf7ce1c6aa3a71e47c5709a679c96a
kylebystrom <kylebystrom@gmail.com>
2024-09-23 21:20:06 -0400

    fix package spec

--- .github/workflows/mm_install.sh
+++ .github/workflows/mm_install.sh
@@ -1,4 +1,4 @@
-micromamba install mkl<=2024.0 mkl-devel<=2024.0 mkl-service<=2024.0 mkl_fft mkl_random
+micromamba install mkl"<=2024.0" mkl-devel"<=2024.0" mkl-service"<=2024.0" mkl_fft mkl_random
 micromamba install openmpi openmpi-mpicc
 micromamba install libxc conda-forge::fftw
 micromamba install pytorch pytorch-cuda=11.8 -c pytorch -c nvidia
