1eaca6c926d818365299bd0c3bce895178f378f5
kylebystrom <kylebystrom@gmail.com>
2025-02-28 17:31:46 -0500

    make sure to download the functionals in CI tests

--- .github/workflows/run_core_ci.sh
+++ .github/workflows/run_core_ci.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sh .github/workflows/apt_deps.sh
 pip install .
+python scripts/download_functionals.py
 sh .github/workflows/run_tests.sh
--- .github/workflows/run_gpaw_ci.sh
+++ .github/workflows/run_gpaw_ci.sh
@@ -4,4 +4,5 @@ pip install gpaw
 gpaw install-data --sg15 --register $PWD
 gpaw install-data --register $PWD
 pip install .
+python scripts/download_functionals.py
 sh .github/workflows/run_gpaw_tests.sh
