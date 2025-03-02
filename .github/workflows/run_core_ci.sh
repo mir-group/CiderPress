e4a9fe7900613cd7d9b51fbcb66937df72f73cd7
kylebystrom <kylebystrom@gmail.com>
2025-03-02 17:40:28 -0500

    fix typo and install pytest

--- .github/workflows/run_core_ci.sh
+++ .github/workflows/run_core_ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sh .github/workflows/apt_deps.sh
+pip install pytest
 pip install .
 python scripts/download_functionals.py
 sh .github/workflows/run_tests.sh
--- .github/workflows/run_gpaw_ci.sh
+++ .github/workflows/run_gpaw_ci.sh
@@ -3,6 +3,7 @@ sh .github/workflows/apt_deps.sh
 pip install gpaw
 gpaw install-data --sg15 --register $PWD
 gpaw install-data --register $PWD
+pip install pytest
 pip install .
 python scripts/download_functionals.py
 sh .github/workflows/run_gpaw_tests.sh
