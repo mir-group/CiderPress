770adb9c28d018d4a76c88fa1c85ca261973cad7
kylebystrom <kylebystrom@gmail.com>
2025-03-02 18:08:09 -0500

    same fix but for the mpi tests

--- .github/workflows/run_gpaw_mpi_ci.sh
+++ .github/workflows/run_gpaw_mpi_ci.sh
@@ -4,6 +4,14 @@ sudo apt-get -qq install openmpi-bin openmpi-common libopenmpi-dev
 sudo apt-get -qq install libscalapack-openmpi-dev
 pip install gpaw
 
+CMAKE_CONFIGURE_ARGS="-DBUILD_LIBXC=on" pip install .
+python scripts/download_functionals.py
+
+# Slightly hacky, link gpaw to our internal libxc
+export CIDER_DEPS=$(python -c "import ciderpress, os; print(os.path.dirname(ciderpress.__file__))")/lib/deps
+export LIBRARY_PATH=$LIBRARY_PATH:$CIDER_DEPS/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CIDER_DEPS/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:$CIDER_DEPS/include
 export CIDERDIR=$PWD
 cd ..
 git clone https://gitlab.com/gpaw/gpaw.git
@@ -15,6 +23,4 @@ gpaw install-data --sg15 --register $PWD
 gpaw install-data --register $PWD
 cd $CIDERDIR
 
-CMAKE_CONFIGURE_ARGS="-DBUILD_LIBXC=on" pip install .
-python scripts/download_functionals.py
 sh .github/workflows/run_gpaw_tests.sh
