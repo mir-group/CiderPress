8f86c55ea3cd27d7509ca9b33ab66ad9d9d06a00
kylebystrom <kylebystrom@gmail.com>
2025-03-02 19:14:07 -0500

    try to run gpaw mpi without scalapack

--- .github/workflows/gpaw_blas_siteconfig.py
+++ .github/workflows/gpaw_blas_siteconfig.py
@@ -19,7 +19,7 @@
 #
 
 
-scalapack = True
+scalapack = False
 mpi = True
 fftw = True
 compiler = "mpicc"
@@ -31,15 +31,8 @@ libraries = [
     "mpi",
     "blas",
     "lapack",
-    "scalapack-openmpi",
     "gomp",
     "pthread",
 ]
 
 extra_compile_args += ["-O3", "-std=c99", "-w", "-m64"]
-
-define_macros += [
-    ("GPAW_NO_UNDERSCORE_CBLACS", "0"),
-    ("GPAW_NO_UNDERSCORE_CSCALAPACK", "0"),
-    ("GPAW_NO_UNDERSCORE_SCALAPACK", "0"),
-]
