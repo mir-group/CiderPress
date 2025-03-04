e7d4e7a709cf093cea1c841f6325ea996fbe2867
Kyle Bystrom <kylebystrom@gmail.com>
2025-03-04 14:34:39 -0500

    revert unit test change

--- .github/workflows/run_gpaw_tests.sh
+++ .github/workflows/run_gpaw_tests.sh
@@ -4,5 +4,4 @@ export PYTHONPATH=$(pwd):$PYTHONPATH
 ulimit -s 20000
 
 version=$(python -c 'import sys; print("{0}.{1}".format(*sys.version_info[:2]))')
-python -m unittest ciderpress.gpaw.tests.test_descriptors.TestDescriptors.test_eigval_vs_fd
 pytest ciderpress/gpaw -s -c pytest_mpi.ini ciderpress
