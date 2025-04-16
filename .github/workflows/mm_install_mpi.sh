275e1da9f23b5ffb129a64d9c0d34f206774b763
Kyle Bystrom <kylebystrom@gmail.com>
2025-04-16 19:12:13 -0400

    change mkl version

--- .github/workflows/mm_install_mpi.sh
+++ .github/workflows/mm_install_mpi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp .github/workflows/nodef_condarc ~/.condarc
-micromamba install mkl"<=2024.0" mkl-devel"<=2024.0" mkl-service"<=2024.0" mkl_fft mkl_random
+micromamba install mkl mkl-devel mkl-service mkl_fft mkl_random
 micromamba install openmpi openmpi-mpicc
 micromamba install libxc conda-forge::fftw
